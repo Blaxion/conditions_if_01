defd20158791607a17c01c89ef4b790fffeeaf9f
Blaxion <khibaziev.f@gmail.com>
2021-04-20 22:17:41 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,105 @@
+// // 1.
+// console.log(1 == "1");
+
+
+// // 2.
+// console.log(1 === "1");
+
+
+// // 3.
+// let name = prompt("What's your name, 5 char at least");
+
+// if (name.length < 5 ) {
+//     alert("ton nom est trop cours")
+// }
+
+
+// // 4.
+// let x = prompt("2 + 2 = ?");
+// if (x == 2 + 2){
+//     alert("goodjob")
+// }
+// else {
+//     alert("teubé")
+// }
+
+
+// // 5.
+// let x = prompt("2 * 2 = ?");
+// if (x == 4){
+//     alert("goodjob")
+// }
+// else {
+//     alert( "C'est dommage, tu n'étais qu'a "+ Math.abs(4 - x) +" unité de la bonne réponse")
+// }
+
+
+// // 6.
+// if (tab.length !=3 ) {
+//     tab.push(prompt("C'est quoi ton 1 élément?"))
+// }
+// else{
+//     alert(tab)
+// }
+
+// if (tab.length !=3 ) {
+//     tab.push(prompt("C'est quoi ton 2 élément?"))
+// }
+// else{
+//     alert(tab)
+// }
+
+// if (tab.length !=3) {
+//     tab.push(prompt("C'est quoi ton 3 élément?"))
+// }
+// else{
+//     alert(tab)
+// }
+
+// if (tab.length !=3) {
+//     tab.push(prompt("C'est quoi ton X élément?"))
+// }
+// else{
+//     alert(tab)
+// }
+
+
+// // 7.
+// let ptbac = [];
+// let gdbac = [];
+
+// let temp=prompt("C'est quoi ton chiffres ?")
+// if (temp > 12 ) {
+//     gdbac.push(temp)
+// } else {
+//     ptbac.push(temp)
+// }
+// alert(ptbac+"\n"+gdbac)
+
+// temp=prompt("C'est quoi ton chiffres ?")
+// if (temp > 12 ) {
+//     gdbac.push(temp)
+// } else {
+//     ptbac.push(temp)
+// }
+// console.log(gdbac, ptbac)
+// alert(ptbac+"\n"+gdbac)
+
+// temp=prompt("C'est quoi ton chiffres ?")
+// if (temp > 12 ) {
+//     gdbac.push(temp)
+// } else {
+//     ptbac.push(temp)
+// }
+// console.log(gdbac, ptbac)
+// alert(ptbac+"\n"+gdbac)
+
+// temp=prompt("C'est quoi ton chiffres ?")
+// if (temp > 12 ) {
+//     gdbac.push(temp)
+// } else {
+//     ptbac.push(temp)
+// }
+// console.log(gdbac, ptbac)
+// alert(ptbac+"\n"+gdbac)
+
